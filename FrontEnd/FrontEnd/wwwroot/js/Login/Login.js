ecc6704d9e3132124d8709f0cfb0bb0d4a6a387d
eabantoxternal <89857181+eabantoxternal@users.noreply.github.com>
2023-03-03 02:48:38 -0500

    LoginAcceso

---
+++ FrontEnd/FrontEnd/wwwroot/js/Login/Login.js
@@ -0,0 +1,32 @@
+﻿
+
+function LoginAcceso() {
+
+
+    var user = document.getElementById("txtcorreo").value;
+    var pass = document.getElementById("txtcontrasena").value;
+
+
+    fetch(UrlBack + "Login/UserLogin", {
+        method: 'POST',
+        body: JSON.stringify({
+
+            "credential": user,
+            "password": pass
+
+        }),
+        headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+
+    })
+        .then(function (response) { return response.json(); })
+        .catch(error => {
+            console.log(error);
+        });
+        //.then(res => {
+        //    console.log("ok");
+        //})
+        //.catch(error => {
+        //    console.log(error);
+        //});
+}
+
